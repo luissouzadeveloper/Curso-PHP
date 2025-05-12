93e618c5550453c4b97c4234c47c77d2d3aecb54
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-12 18:08:37 -0300

    Embutindo tag php no html - Aula 01

---
+++ PHP/ling-php/aula01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+    <!-- Embutindo PHP no HTML -->
+    <?php 
+        echo 'Curso de PHP';
+    ?>
+    <br>
+    <?= 'Impressão PHP' ?>
+</body>
+</html>
