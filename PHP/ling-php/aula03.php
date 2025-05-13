f7c7e83f0e875e669967cfabb47b685239b9fa4b
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-13 09:23:58 -0300

    Comentários - Aula 03

---
+++ PHP/ling-php/aula03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentários</title>
+</head>
+<body>
+    <?php 
+        echo "COMENTÁRIOS <br>";
+        // Comentário de uma linha 
+        echo "// <- Uma linha <br>";
+
+        /*
+            Comentário de múltiplas linhas
+        */
+        echo "/* */ <- Múltiplas linhas";
+    ?>
+</body>
+</html>
