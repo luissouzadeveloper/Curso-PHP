c526ab98166d5664e162bba33bf1b8cad609d291
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-12 23:14:30 -0300

    Comandos de saída - Aula 02

---
+++ PHP/ling-php/aula02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comandos de saída</title>
+</head>
+<body>
+    <!-- Comandos de saída no php -->
+     <?php 
+        echo "Hello World!<br>";
+        print "Hello World 2 <hr>";
+     ?>
+</body>
+</html>
