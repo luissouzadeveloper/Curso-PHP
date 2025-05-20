bff6e54bad5be90e23b46c9a5baf4b0a44bba54b
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-20 08:35:11 -0300

    Condicionais parte 1 - Aula 07

---
+++ PHP/ling-php/aula07.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 07</title>
+</head>
+<body>
+    <?php 
+        if ('x' != 'y') {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+        
+    ?>
+</body>
+</html>
