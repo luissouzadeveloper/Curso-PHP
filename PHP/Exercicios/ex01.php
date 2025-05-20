5e10c3cd7731a2cefacad63fcf082995c55ff6e6
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-20 10:59:18 -0300

    Exercício 01

---
+++ PHP/Exercicios/ex01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex 01</title>
+</head>
+<body>
+    <?php 
+        // Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+        echo "Olá, Mundo!";
+    ?>
+</body>
+</html>
