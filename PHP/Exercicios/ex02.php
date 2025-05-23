ec404895501ff6b8c87c6abea7381461f56f1674
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-23 10:04:37 -0300

    Exercício 02

---
+++ PHP/Exercicios/ex02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 02</title>
+</head>
+<body>
+    <?php 
+        $nome = 'João';
+
+        echo "Bem vindo $nome";
+    ?>
+
+    <form action=""></form>
+</body>
+</html>
