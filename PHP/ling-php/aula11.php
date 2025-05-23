4b76e2dc533f1e19bf743f262022b607c88bdac5
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-05-23 09:57:37 -0300

    Operador ternário - Aula 11

---
+++ PHP/ling-php/aula11.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 11</title>
+</head>
+<body>
+    <?php 
+        // Operador ternário
+        $idade = 17;
+        $maiorIdade = $idade >= 18 ? 'Maior de idade' : 'Menor de idade';
+
+        echo $maiorIdade;
+    ?>
+</body>
+</html>
