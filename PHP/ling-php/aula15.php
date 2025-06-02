a8414e50a55ebf757550ba8c8c5ca86c13498e8e
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-01 22:58:17 -0300

    Operador de atribuição com operadores aritméticos - Aula 15

---
+++ PHP/ling-php/aula15.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 15</title>
+</head>
+<body>
+    <?php 
+        // Operador de atribuição com operadores aritméticos
+        $num = 10;
+        // $num = $num + 8;
+        $num += 8; // <- forma simplificada!
+
+        echo $num;
+    ?>
+</body>
+</html>
