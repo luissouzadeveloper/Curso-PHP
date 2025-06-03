552145383337b09bd26fe97a4af3d77b9644ae8a
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-03 09:50:44 -0300

    Exercício 05

---
+++ PHP/Exercicios/ex05.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 05</title>
+</head>
+<body>
+    <?php 
+        /*
+            Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório entre eles.
+            Ex:
+            Digite um valor: 8
+            Digite outro valor: 5
+            A soma entre 8 e 5 é igual a 13.
+        */
+
+        $num1 = 12;
+        $num2 = 40;
+
+        $soma = $num1 + $num2;
+
+        echo "A soma entre $num1 e $num2 é $soma";
+    ?>
+</body>
+</html>
