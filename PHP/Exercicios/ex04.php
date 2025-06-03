0ed094c7ed382aa71096060efd77ca5f7719817e
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-03 09:50:28 -0300

    Exercício 04

---
+++ PHP/Exercicios/ex04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 04</title>
+</head>
+<body>
+    <?php 
+        /*
+            Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+            Ex:
+            Nome do Funcionário: Maria do Carmo
+            Salário: 1850,45
+            O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+        */
+
+        $nome = 'José Silva';
+        $salario = 1.518;
+
+        echo "O funcionário(a) $nome tem um salário de R$$salario em Junho!";
+    ?>
+</body>
+</html>
