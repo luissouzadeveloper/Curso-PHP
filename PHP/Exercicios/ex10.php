2833adfe3b2019ba4602ce6d5d15bacaf43f9677
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-05 09:08:44 -0300

    Exercício 10

---
+++ PHP/Exercicios/ex10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <?php 
+        /*
+            Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
+        */
+
+        $reais = 10;
+        $dolar = $reais / 5.63;
+
+        echo "Você tem R$$reais e em dólar é $$dolar";
+    ?>
+</body>
+</html>
